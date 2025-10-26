84c83aec91710525b25d1c26250dbcabe9216f2a
Veniamin Nekhoda <venimain.nekhoda@gmail.com>
2025-10-26 20:27:17 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import os
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
         os.remove(self.filename)
