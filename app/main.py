781e97ba1b3a7820f8b846451c48448a7928755a
Veniamin Nekhoda <venimain.nekhoda@gmail.com>
2025-10-27 11:35:39 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> CleanUpFile:
+    def __enter__(self) -> Any:
         return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
